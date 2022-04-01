0df10245533a0f5f8b1942ec980fd348df3578a4
RomanReznichenko <59834081+RomanReznichenko@users.noreply.github.com>
2022-04-01 16:49:26 +0300

    Update packages/core/src/bus/Exceptions/IllegalOperation.ts
    Co-authored-by: Artem Derevnjuk <artem.derevnjuk@neuralegion.com>

--- packages/core/src/bus/Exceptions/IllegalOperation.ts
+++ packages/core/src/bus/Exceptions/IllegalOperation.ts
@@ -4,7 +4,7 @@ import { EventDispatcher } from '../EventDispatcher';
 export class IllegalOperation extends Error {
   constructor(instance: EventDispatcher | CommandDispatcher) {
     super(
-      `Please make sure that ${instance.constructor.name} established a connection with host..`
+      `Please make sure that ${instance.constructor.name} established a connection with host.`
     );
     this.name = new.target.name;
     Object.setPrototypeOf(this, new.target.prototype);
