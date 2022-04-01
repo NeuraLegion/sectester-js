31371b43f838fc4384a7e8fb6946c9486a6f44f1
Reznichenko Roman <roma.reznschenko.95@gmail.com>
2022-04-01 16:46:30 +0300

    refactor(*): move ConnectionFactory to bus package

--- packages/bus/src/ConectionFactory.ts
+++ packages/bus/src/ConectionFactory.ts
@@ -1,5 +1,5 @@
 export interface ConnectionFactory<T> {
-  create(url: string, options?: any): Promise<T>;
+  create(url: string, options?: Record<string, unknown>): Promise<T>;
 }
 
 export const ConnectionFactory: unique symbol = Symbol('ConnectionFactory');
--- packages/core/src/bus/EventHandler.ts
+++ packages/core/src/bus/EventHandler.ts
@@ -1,6 +1,3 @@
-import { Event } from './Event';
-import { Handler } from './Handler';
-
-export type EventHandler<T> = Handler<T, any>;
-
-export declare type ExecutionResult<T> = Event<T> | unknown;
+export interface EventHandler<T, R = void> {
+  handle(payload: T): Promise<R | undefined>;
+}
