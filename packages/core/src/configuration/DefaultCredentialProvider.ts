9ade90259d231080358c9f7b0b58420d2ab19048
Reznichenko Roman <roma.reznschenko.95@gmail.com>
2022-04-01 16:33:41 +0300

    fix(*): update return data of CredentialsProvider

--- packages/core/src/configuration/DefaultCredentialProvider.ts
+++ packages/core/src/configuration/DefaultCredentialProvider.ts
@@ -1,12 +1,10 @@
 import { CredentialProvider, Credentials } from './CredentialsProvider';
-import { injectable } from 'tsyringe';
 import { env } from 'process';
 
-@injectable()
 export class EnvCredentialProvider implements CredentialProvider {
   public get(): Promise<Credentials | undefined> {
     const token = (env as any).BRIGHT_TOKEN;
 
-    return Promise.resolve(token);
+    return Promise.resolve({ token } as Credentials);
   }
 }
