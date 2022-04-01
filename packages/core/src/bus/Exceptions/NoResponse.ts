19978cdb7f64f1c13f8ddf4dc0911107e56b99ca
RomanReznichenko <59834081+RomanReznichenko@users.noreply.github.com>
2022-04-01 16:49:27 +0300

    Update packages/core/src/bus/Exceptions/NoResponse.ts
    Co-authored-by: Artem Derevnjuk <artem.derevnjuk@neuralegion.com>

--- packages/core/src/bus/Exceptions/NoResponse.ts
+++ packages/core/src/bus/Exceptions/NoResponse.ts
@@ -1,6 +1,6 @@
 export class NoResponse extends Error {
   constructor(duration: number) {
-    super(`No response for ${duration}seconds.`);
+    super(`No response for ${duration} seconds.`);
     this.name = new.target.name;
     Object.setPrototypeOf(this, new.target.prototype);
   }
