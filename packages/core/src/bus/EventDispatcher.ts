17569123e823adbf297904dd1db05ef5bf79b82d
RomanReznichenko <59834081+RomanReznichenko@users.noreply.github.com>
2022-04-01 16:49:05 +0300

    Update packages/core/src/bus/EventDispatcher.ts
    Co-authored-by: Artem Derevnjuk <artem.derevnjuk@neuralegion.com>

--- packages/core/src/bus/EventDispatcher.ts
+++ packages/core/src/bus/EventDispatcher.ts
@@ -3,3 +3,6 @@ import { Event } from './Event';
 export interface EventDispatcher {
   publish<T>(event: Event<T>): Promise<void>;
 }
+
+export const EventDispatcher: unique symbol = Symbol('EventDispatcher');
+
