b34e64bffca2cf611bde3a28f176e8cc4cd706d9
Reznichenko Roman <roma.reznschenko.95@gmail.com>
2022-04-01 16:35:53 +0300

    test(*): add tests for DeafaultCredentialsProvider

---
+++ packages/core/src/credentials-provider/DefaultCredentialsProvider.spec.ts
@@ -0,0 +1,42 @@
+import { CredentialProvider } from './CredentialsProvider';
+import { EnvCredentialProvider } from './DefaultCredentialProvider';
+
+describe('EnvCredentialProvider', () => {
+  it('sholud create new instance', () => {
+    const provider = new EnvCredentialProvider();
+    expect(provider).toBeDefined();
+  });
+
+  describe('get', () => {
+    const OLD_ENV = process.env;
+    let provider: CredentialProvider;
+
+    beforeAll(() => {
+      provider = new EnvCredentialProvider();
+    });
+
+    beforeEach(() => {
+      jest.resetModules();
+      process.env = { ...OLD_ENV };
+    });
+
+    afterAll(() => {
+      process.env = OLD_ENV;
+    });
+
+    it('should return undefuned if credentials in nit set', async () => {
+      const token = (await provider.get())?.token;
+
+      expect(token).toBeUndefined();
+    });
+
+    it('should return credentials from environment', async () => {
+      const testToken = 'test-token';
+      (process.env as any).BRIGHT_TOKEN = testToken;
+
+      const token = (await provider.get())?.token;
+
+      expect(token).toEqual(testToken);
+    });
+  });
+});
