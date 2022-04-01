aff163d12857be616d75e6440e05aae74dc9f936
RomanReznichenko <59834081+RomanReznichenko@users.noreply.github.com>
2022-04-01 16:49:29 +0300

    Update packages/core/src/bus/CommandDispatcher.ts
    Co-authored-by: Artem Derevnjuk <artem.derevnjuk@neuralegion.com>

--- packages/core/src/bus/CommandDispatcher.ts
+++ packages/core/src/bus/CommandDispatcher.ts
@@ -3,3 +3,5 @@ import { Command } from './Command';
 export interface CommandDispatcher {
   execute<T, R>(command: Command<T, R>): Promise<R | undefined>;
 }
+
+export const CommandDispatcher: unique symbol = Symbol('CommandDispatcher');
