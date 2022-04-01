e852324157fadc51b1b9b73697c9baeec6aaf64c
RomanReznichenko <59834081+RomanReznichenko@users.noreply.github.com>
2022-04-01 16:49:27 +0300

    Update packages/core/src/bus/Exceptions/IligalHandler.ts
    Co-authored-by: Artem Derevnjuk <artem.derevnjuk@neuralegion.com>

--- packages/core/src/bus/Exceptions/IligalHandler.ts
+++ packages/core/src/bus/Exceptions/IligalHandler.ts
@@ -1,6 +1,6 @@
-export class IligalHandler extends Error {
-  constructor() {
-    super(`Can't find event bound to handler. Please use 'bind' decorator`);
+export class NoSubscriptionsFound extends Error {
+  constructor(handler: EventHandler<unknown, unknown>) {
+    super(`No subscriptions found. Please use '@bind()' decorator to subscribe ${handler.constructor.name} to events.`);
     this.name = new.target.name;
     Object.setPrototypeOf(this, new.target.prototype);
   }
