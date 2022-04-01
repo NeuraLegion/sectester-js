d9b3b6a916ec7d320f516a13c0326781883dbe1b
Reznichenko Roman <roma.reznschenko.95@gmail.com>
2022-04-01 16:35:51 +0300

    fix(*): move CredentialsProvider to another folder

--- packages/core/src/configuration/CredentialsProvider.ts
+++
@@ -1,9 +0,0 @@
-export interface Credentials {
-  readonly token: string;
-}
-
-export interface CredentialProvider {
-  get(): Promise<Credentials | undefined>;
-}
-
-export const CredentialProvider: unique symbol = Symbol('CredentialProvider');
