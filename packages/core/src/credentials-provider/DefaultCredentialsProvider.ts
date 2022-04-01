9dbfe58c1213a470515e00a4896f227ea5c81b36
Reznichenko Roman <roma.reznschenko.95@gmail.com>
2022-04-01 16:40:33 +0300

    fix(*): rename env credentials provider

--- packages/core/src/credentials-provider/DefaultCredentialsProvider.ts
+++
@@ -1,9 +0,0 @@
-import { CredentialProvider, Credentials } from './CredentialsProvider';
-
-export class EnvCredentialProvider implements CredentialProvider {
-  public get(): Promise<Credentials | undefined> {
-    const token = process.env['BRIGHT_TOKEN'];
-
-    return Promise.resolve(token ? { token } : undefined);
-  }
-}
