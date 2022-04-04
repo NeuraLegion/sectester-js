a73474b8f1306f592cf65404d5f269563e8583ad
Reznichenko Roman <roma.reznschenko.95@gmail.com>
2022-04-04 10:11:58 +0300

    test(core): add derived classes

--- packages/core/src/utils/get-type-name.spec.ts
+++ packages/core/src/utils/get-type-name.spec.ts
@@ -1,10 +1,26 @@
+/* eslint-disable max-classes-per-file */
+import { Command, Event } from '../bus';
 import { getTypeName } from './get-type-name';
 
 class Test {}
 
+class TestEvent extends Event<string> {
+  constructor() {
+    super('TestEvent');
+  }
+}
+
+class TestCommand extends Command<string, void> {
+  constructor() {
+    super('TestCommand');
+  }
+}
+
 describe('getTypeName', () => {
   it.each([
     { input: new Test(), expected: 'Test' },
+    { input: new TestEvent(), expected: 'TestEvent' },
+    { input: new TestCommand(), expected: 'TestCommand' },
     { input: new Date(), expected: 'Date' },
     { input: 5, expected: 'Number' },
     { input: '', expected: 'String' }
