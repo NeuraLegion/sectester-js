924b564f170e19d38775e0b96fda6d053304dabb
Viachaslau Tyshkavets <pmstss@gmail.com>
2022-04-18 22:10:49 +0400

    test(repeater): unit tests for `RepeaterFactory`
    relates-to #22

---
+++ packages/repeater/src/lib/RepeaterFactory.spec.ts
@@ -0,0 +1,49 @@
+import 'reflect-metadata';
+import { RepeaterFactory } from './RepeaterFactory';
+import { Repeater } from './Repeater';
+import { RepeatersManager } from '../api';
+import { EventBusFactory } from '../bus';
+import { Configuration, EventBus } from '@secbox/core';
+import { anything, instance, mock, when } from 'ts-mockito';
+import { DependencyContainer } from 'tsyringe';
+
+describe('RepeaterFactory', () => {
+  const repeaterId = 'fooId';
+
+  const MockedContainer = mock<DependencyContainer>();
+  const MockedConfiguration = mock<Configuration>();
+  const MockedRepeaterManager = mock<RepeatersManager>();
+  const MockedEventBusFactory = mock<EventBusFactory>();
+
+  when(MockedContainer.resolve<EventBusFactory>(EventBusFactory)).thenReturn(
+    instance(MockedEventBusFactory)
+  );
+  when(MockedContainer.resolve<RepeatersManager>(RepeatersManager)).thenReturn(
+    instance(MockedRepeaterManager)
+  );
+
+  when(MockedEventBusFactory.create(anything())).thenResolve(
+    {} as unknown as EventBus
+  );
+
+  when(MockedRepeaterManager.createRepeater(anything())).thenResolve({
+    repeaterId
+  });
+
+  when(MockedConfiguration.container).thenReturn(instance(MockedContainer));
+
+  const mockedConfiguration = instance(MockedConfiguration);
+
+  describe('createRepeater', () => {
+    it('should create repeater', async () => {
+      const factory = new RepeaterFactory(mockedConfiguration);
+
+      const res = await factory.createRepeater();
+
+      expect(res).toBeInstanceOf(Repeater);
+      expect(res).toMatchObject({
+        repeaterId
+      });
+    });
+  });
+});
