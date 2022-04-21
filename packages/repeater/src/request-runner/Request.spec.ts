f057c408ec81e8799d40f8d0f5b3f3b21b9cc9c7
Viachaslau Tyshkavets <pmstss@gmail.com>
2022-04-21 18:07:01 +0400

    test(repeater): requested changes for request/response specs naming conventions
    relates-to #22

--- packages/repeater/src/request-runner/Request.spec.ts
+++ packages/repeater/src/request-runner/Request.spec.ts
@@ -1,74 +1,91 @@
 import { Request } from './Request';
 
 describe('Request', () => {
-  it('should normalize method', () => {
-    expect(
-      new Request({
+  describe('constructor', () => {
+    it('should throw Error on empty url', () => {
+      expect(
+        () =>
+          new Request({
+            url: ''
+          })
+      ).toThrow('Url must be declared explicitly.');
+    });
+
+    it('should throw Error on invalid url', () => {
+      expect(
+        () =>
+          new Request({
+            url: 'http::/foo.bar'
+          })
+      ).toThrow('Invalid URL.');
+    });
+
+    it('should throw Error on invalid body', () => {
+      expect(
+        () =>
+          new Request({
+            url: 'http://foo.bar',
+            body: 42 as unknown as string
+          })
+      ).toThrow('Body must be string.');
+    });
+
+    it('should throw Error on invalid correlationIdRegex', () => {
+      expect(
+        () =>
+          new Request({
+            url: 'http://foo.bar',
+            correlationIdRegex: '('
+          })
+      ).toThrow('Correlation id must be regular expression.');
+    });
+
+    it('should create an instance', () => {
+      expect(
+        () =>
+          new Request({
+            url: 'http://foo.bar'
+          })
+      ).not.toThrow();
+    });
+  });
+
+  describe('method', () => {
+    it('should normalize method', () => {
+      expect(
+        new Request({
+          url: 'http://foo.bar',
+          method: 'post'
+        }).method
+      ).toBe('POST');
+    });
+  });
+
+  describe('url', () => {
+    it('should normalize url', () => {
+      expect(
+        new Request({
+          url: 'HTTP://foo.BAR',
+          method: 'post'
+        }).url
+      ).toBe('http://foo.bar/');
+    });
+  });
+
+  describe('setHeaders', () => {
+    it('should append headers', () => {
+      const request = new Request({
         url: 'http://foo.bar',
-        method: 'post'
-      }).method
-    ).toBe('POST');
-  });
+        headers: { 'x-key': 'value' }
+      });
 
-  it('should normalize url', () => {
-    expect(
-      new Request({
-        url: 'HTTP://foo.BAR',
-        method: 'post'
-      }).url
-    ).toBe('http://foo.bar/');
-  });
+      request.setHeaders({ 'x-a1': 'a1', 'x-a2': 'a2' });
 
-  it('should throw Error on empty url', () => {
-    expect(
-      () =>
-        new Request({
-          url: ''
-        })
-    ).toThrow('Url must be declared explicitly.');
-  });
-
-  it('should append headers', () => {
-    const request = new Request({
-      url: 'http://foo.bar',
-      headers: { 'x-key': 'value' }
+      expect(request.headers).toEqual({
+        'x-key': 'value',
+        'x-a1': 'a1',
+        'x-a2': 'a2'
+      });
     });
-
-    request.setHeaders({ 'x-a1': 'a1', 'x-a2': 'a2' });
-
-    expect(request.headers).toEqual({
-      'x-key': 'value',
-      'x-a1': 'a1',
-      'x-a2': 'a2'
-    });
-  });
-
-  it('should throw Error on invalid url', () => {
-    expect(
-      () =>
-        new Request({
-          url: 'http::/foo.bar'
-        })
-    ).toThrow('Invalid URL.');
-  });
-
-  it('should throw Error on invalid body', () => {
-    expect(
-      () =>
-        new Request({
-          url: 'http://foo.bar',
-          body: 42 as unknown as string
-        })
-    ).toThrow('Body must be string.');
-  });
-
-  it('should throw Error on invalid correlationIdRegex', () => {
-    expect(
-      () =>
-        new Request({
-          url: 'http://foo.bar',
-          correlationIdRegex: '('
-        })
-    ).toThrow('Correlation id must be regular expression.');
   });
 });
--- packages/repeater/src/request-runner/Response.spec.ts
+++ packages/repeater/src/request-runner/Response.spec.ts
@@ -2,36 +2,48 @@ import { Response } from './Response';
 import { Protocol } from '../models';
 
 describe('Response', () => {
-  it('should be possible to create response having only protocol', () => {
-    const responseOptions = {
-      protocol: Protocol.WS
-    };
+  describe('constructor', () => {
+    it('should create an instance having only protocol', () => {
+      const responseOptions = {
+        protocol: Protocol.WS
+      };
 
-    const response = new Response(responseOptions);
+      const response = new Response(responseOptions);
 
-    const { protocol, statusCode, headers, body, message, errorCode } =
-      response;
-    expect({ protocol, statusCode, headers, body, message, errorCode }).toEqual(
-      responseOptions
-    );
-  });
+      const { protocol, statusCode, headers, body, message, errorCode } =
+        response;
+      expect({
+        protocol,
+        statusCode,
+        headers,
+        body,
+        message,
+        errorCode
+      }).toEqual(responseOptions);
+    });
 
-  it('should be possible create response with full fieldset', () => {
-    const responseOptions = {
-      protocol: Protocol.WS,
-      statusCode: 200,
-      headers: { 'x-key': ['x-value'] },
-      body: '{}',
-      message: 'OK',
-      errorCode: 'NO_ERROR'
-    };
+    it('should create an instance with full fieldset', () => {
+      const responseOptions = {
+        protocol: Protocol.WS,
+        statusCode: 200,
+        headers: { 'x-key': ['x-value'] },
+        body: '{}',
+        message: 'OK',
+        errorCode: 'NO_ERROR'
+      };
 
-    const response = new Response(responseOptions);
+      const response = new Response(responseOptions);
 
-    const { protocol, statusCode, headers, body, message, errorCode } =
-      response;
-    expect({ protocol, statusCode, headers, body, message, errorCode }).toEqual(
-      responseOptions
-    );
+      const { protocol, statusCode, headers, body, message, errorCode } =
+        response;
+      expect({
+        protocol,
+        statusCode,
+        headers,
+        body,
+        message,
+        errorCode
+      }).toEqual(responseOptions);
+    });
   });
 });
