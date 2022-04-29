a609254b427516e222b1a90c9293f4c241b60d76
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-04-29 15:04:30 +0400

    test(core): extend test data to cover new cases

--- packages/core/src/utils/contains.spec.ts
+++ packages/core/src/utils/contains.spec.ts
@@ -12,10 +12,14 @@ describe('contains', () => {
   }
 
   it.each([
-    { type: TestEnum2, input: TestEnum2.VALUE1, expected: true },
     { type: TestEnum1, input: TestEnum1.VALUE1, expected: true },
-    { type: TestEnum1, input: 'value3', expected: false },
-    { type: TestEnum2, input: 3, expected: false }
+    { type: TestEnum1, input: 'value2', expected: true },
+    { type: TestEnum1, input: 'VALUE2', expected: false },
+    { type: TestEnum2, input: TestEnum2.VALUE1, expected: true },
+    { type: TestEnum2, input: 0, expected: true },
+    { type: TestEnum2, input: 2, expected: false },
+    { type: TestEnum2, input: 'VALUE1', expected: true },
+    { type: TestEnum2, input: 'VALUE3', expected: false }
   ])(
     'should return $expected if $input is passed ($type)',
     ({ type, input, expected }) => {
