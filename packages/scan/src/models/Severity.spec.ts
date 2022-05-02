1f300326dceda1d4480b3c532ddffb6977788883
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-05-02 15:09:49 +0400

    test(scan): add tests for `Severity`
    relates-to #45

---
+++ packages/scan/src/models/Severity.spec.ts
@@ -0,0 +1,52 @@
+import { Severity, severityComparator, severityToNumber } from './Severity';
+
+describe('Severity', () => {
+  describe('severityComparator', () => {
+    it.each([
+      { input: { a: Severity.LOW, b: Severity.LOW }, expected: 0 },
+      { input: { a: Severity.MEDIUM, b: Severity.MEDIUM }, expected: 0 },
+      { input: { a: Severity.HIGH, b: Severity.HIGH }, expected: 0 },
+      { input: { a: Severity.HIGH, b: Severity.LOW }, expected: -2 },
+      { input: { a: Severity.HIGH, b: Severity.MEDIUM }, expected: -1 },
+      { input: { a: Severity.LOW, b: Severity.HIGH }, expected: 2 },
+      { input: { a: Severity.MEDIUM, b: Severity.HIGH }, expected: 1 },
+      { input: { a: Severity.MEDIUM, b: Severity.LOW }, expected: -1 },
+      { input: { a: Severity.LOW, b: Severity.MEDIUM }, expected: 1 }
+    ])(
+      'should return a distance (i.e $expected) between $input.a and $input.b',
+      ({ input, expected }) => {
+        // act
+        const result = severityComparator(input.a, input.b);
+
+        // assert
+        expect(result).toEqual(expected);
+      }
+    );
+  });
+
+  describe('severityToNumber', () => {
+    it.each([
+      { input: Severity.LOW, expected: 1 },
+      { input: Severity.MEDIUM, expected: 2 },
+      { input: Severity.HIGH, expected: 3 }
+    ])(
+      'should return a number representation for $input',
+      ({ input, expected }) => {
+        // act
+        const result = severityToNumber(input);
+
+        // assert
+        expect(result).toEqual(expected);
+      }
+    );
+
+    it('should raise an error if supplied input is invalid', () => {
+      const input = 'Critical';
+
+      // act & assert
+      expect(() => severityToNumber(input as unknown as Severity)).toThrow(
+        'Unknown severity value'
+      );
+    });
+  });
+});
