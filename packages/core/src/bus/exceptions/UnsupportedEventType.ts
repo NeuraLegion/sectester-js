80108d7138a1891d8dd423ee7e32c633fac67b92
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-05-17 00:44:41 +0400

    refactor(core): remove duplicates
    closes #88

--- packages/core/src/bus/exceptions/UnsupportedEventType.ts
+++ packages/core/src/bus/exceptions/UnsupportedEventType.ts
@@ -4,6 +4,5 @@ import { getTypeName } from '../../utils';
 export class UnsupportedEventType extends SecTesterError {
   constructor(event: unknown) {
     super(`${getTypeName(event)} cannot be used with the @bind decorator.`);
-    this.name = new.target.name;
   }
 }
