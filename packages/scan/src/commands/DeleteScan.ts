02c78ec4b717627174921dc01e246d23c5855c7a
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2022-05-20 22:15:33 +0400

    feat(scan): allow removing scans

---
+++ packages/scan/src/commands/DeleteScan.ts
@@ -0,0 +1,12 @@
+import { HttpRequest } from '@sec-tester/bus';
+
+export class DeleteScan extends HttpRequest {
+  constructor(id: string) {
+    super({
+      method: 'DELETE',
+      url: `/api/v1/scans/${id}`,
+      payload: undefined,
+      expectReply: false
+    });
+  }
+}
